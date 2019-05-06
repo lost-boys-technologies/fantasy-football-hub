939ed6bf1f68997436993cf557047cd83f36d152
Kenny Howard <ken.chad.howard@gmail.com>
2019-05-05 22:42:28 -0600

    improving router in index

--- src/App.js
+++ src/App.js
@@ -1,55 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Home from './Home'
-import About from './About';
-import Members from './Members';
-import Constitution from './Constitution';
-import Rules from './Rules';
-import Bets from './Bets';
 import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="header-container">
-        <Router>
-          <div className="router-container">
-            <ul className="nav-link-container">
-              <li>
-                <Link to="/" className="nav-link">Home</Link>
-              </li>
-              <li>
-                <Link to="/about" className="nav-link">About Us</Link>
-              </li>
-              <li>
-                <Link to="/members" className="nav-link">Members</Link>
-              </li>
-              <li>
-                <Link to="/constitution" className="nav-link">Constitution</Link>
-              </li>
-              <li>
-                <Link to="/rules" className="nav-link">Rules</Link>
-              </li>
-              <li>
-                <Link to="/bets" className="nav-link">Bets</Link>
-              </li>
-            </ul>
-
-            <hr />
-
-            <Route exact path="/" component={Home} />
-            <Route path="/about" component={About} />
-            <Route path="/members" component={Members} />
-            <Route path="/constitution" component={Constitution} />
-            <Route path="/rules" component={Rules} />
-            <Route path="/bets" component={Bets} />
-
-          </div>
-        </Router>
-      </header>
-
-    </div>
-  );
+    <React.Fragment>
+        <h2>Home</h2>
+        <p>works like a charm</p>
+    </React.Fragment>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Link, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './index.scss';
+import './App.scss';
 import App from './App';
+import About from './About';
+import Members from './Members';
+import Constitution from './Constitution';
+import Rules from './Rules';
+import Bets from './Bets';
+import NotFound from './404';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div className="header-container">
+            <div className="router-container">
+                <ul className="nav-link-container">
+                    <li>
+                        <Link to='/' className="nav-link">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about" className="nav-link">About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/members" className="nav-link">Members</Link>
+                    </li>
+                    <li>
+                        <Link to="/constitution" className="nav-link">Constitution</Link>
+                    </li>
+                    <li>
+                        <Link to="/rules" className="nav-link">Rules</Link>
+                    </li>
+                    <li>
+                        <Link to="/bets" className="nav-link">Bets</Link>
+                    </li>
+                </ul>
+            </div>            
+        </div>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/about" component={About} />
+            <Route path="/members" component={Members} />
+            <Route path="/constitution" component={Constitution} />
+            <Route path="/rules" component={Rules} />
+            <Route path="/bets" component={Bets} />
+            <Route component={NotFound}/>
+        </Switch>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
