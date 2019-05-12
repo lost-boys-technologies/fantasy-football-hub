840b7a9366684c6383ab3d72f68858c4fb8b5847
Kenny Howard <ken.chad.howard@gmail.com>
2019-05-12 00:05:33 -0600

    brining in material ui

--- src/Bets/index.js
+++ src/Bets/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
 import './Bets.scss';
+// import PropTypes from 'prop-types';
+// import classNames from 'classnames';
+// import { withStyles } from '@material-ui/core/styles';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableSortLabel from '@material-ui/core/TableSortLabel';
+// import Paper from '@material-ui/core/Paper';
+// import { AutoSizer, Column, SortDirection, Table } from 'react-virtualized';
+
 
 function Bets() {
   return (
