8e3203582c95c6fe5f9d96da3db5aac0c3291fe8
Kenny Howard <ken.chad.howard@gmail.com>
2019-07-02 02:02:56 -0600

    beginning form process - sign in and up are being duplicated unnecessarily

--- src/components/Auth/SignIn.js
+++ src/components/Auth/SignIn.js
@@ -1,40 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class SignIn extends Component {
-    state = {
-        email: '',
-        password: '',
-    }
+import Form from '../Form';
 
-    handleChange = (e) => {
-        this.setState({ [e.target.id]: e.target.value }) //! grabs the value from the id from the below JSX
-    }
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('this.state', this.state);
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <form className="white" onSubmit={this.handleSubmit}>
-                    <h5 className="grey-text text-darken-3">Sign In</h5>
-                    <div className="input-field">
-                        <label htmlFor="email">Email</label>
-                        <input type="email" id="email" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <label htmlFor="password">Password</label>
-                        <input type="password" id="password" onChange={this.handleChange} />
-                    </div>
-                    <div className="input-field">
-                        <button className="btn pink ligthen-1 z-depth-0">Login</button>
-                    </div>
-                </form>
-            </div>
-        )
-    }
+const SignIn = () => {
+    return (
+        <div>
+            <Form  /> //! WIP
+        </div>
+    )
 }
 
-export default SignIn
+export default SignIn;
