a61b76c3b7b63445cd27e02241803e04527f952c
Kenny Howard <ken.chad.howard@gmail.com>
2019-07-12 01:05:15 -0600

    changed up the overal structure of bets page

---
+++ src/components/Countdown/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Typography } from '@material-ui/core';
+
+export default class Countdown extends Component {
+	render() {
+		return <div>3d 10h 30m</div>;
+	}
+}
