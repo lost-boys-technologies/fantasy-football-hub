56d12a17c085c8d0f34c4145df70125dd2516b5d
Ken Howard <ken.chad.howard@gmail.com>
2019-07-12 01:11:15 -0600

    Different approach (#8)
    * fixing vulnerability
    
    * changed up the overal structure of bets page

--- src/store/reducers/betReducer.js
+++ src/store/reducers/betReducer.js
@@ -1,18 +1,64 @@
 const initState = {
-    // Test Data (temporary)
-    // TODO: Figure out better status value name
-    bets: [
-        {id: 1, status: 'PENDING', pending: true, bettor: 'Evan', takers: ['Kenny', 'Sean']},
-        {id: 2, status: 'COMPLETE', pending: false, bettor: 'Kyle', takers: ['Evan']},
-        {id: 3, status: 'PENDING', pending: false, bettor: 'Evan', takers: ['Kenny', 'Sean', 'Simon', 'Kyle']},
-    ]
+	// Test Data (temporary)
+	// TODO: Figure out better status value name
+	bets: [
+		{
+			id: 1,
+			startDate: 'this',
+			status: 'IN PROGRESS',
+			pending: true,
+			bettor: 'Evan',
+			takers: ['Kenny', 'Sean'],
+		},
+		{
+			id: 2,
+			startDate: 'this',
+			status: 'COMPLETE',
+			pending: false,
+			bettor: 'Kyle',
+			takers: ['Evan'],
+		},
+		{
+			id: 3,
+			startDate: 'this',
+			status: 'IN PROGRESS',
+			pending: false,
+			bettor: 'Evan',
+			takers: ['Kenny', 'Sean', 'Simon', 'Kyle'],
+		},
+		{
+			id: 4,
+			startDate: 'this',
+			status: 'IN PROGRESS',
+			pending: false,
+			bettor: 'Evan',
+			takers: ['Kenny', 'Simon', 'Kyle'],
+		},
+		{
+			id: 5,
+			startDate: 'this',
+			status: 'IN PROGRESS',
+			pending: false,
+			bettor: 'Evan',
+			takers: ['Kenny', 'Sean', 'Simon'],
+		},
+		{
+			id: 6,
+			startDate: 'this',
+			status: 'IN PROGRESS',
+			pending: true,
+			bettor: 'Kenny',
+			takers: ['Sean', 'Simon', 'Kyle'],
+		},
+	],
 };
 
 const betReducer = (state = initState, action) => {
-    switch (action.type) {
-        case 'CREATE_BET' : console.log('created bet', action.bet);
-    }
-    return state;
-}
+	switch (action.type) {
+		case 'CREATE_BET':
+			console.log('created bet', action.bet);
+	}
+	return state;
+};
 
-export default betReducer
+export default betReducer;
