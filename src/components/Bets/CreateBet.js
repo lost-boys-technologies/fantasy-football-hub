d61bdc17061636a8b357cbcdae292112250fa278
Kenny Howard <ken.chad.howard@gmail.com>
2019-07-17 00:35:38 -0600

    mistakes were made

--- src/components/Bets/CreateBet.js
+++ src/components/Bets/CreateBet.js
@@ -1,43 +1,66 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import Button from '@material-ui/core/Button';
 import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import TextField from '@material-ui/core/TextField';
+import { connect } from 'react-redux';
+import { createBet } from '../../store/actions/betActions';
 
-export default function CreateBet(props) {
-	const [values, setValues] = useState({});
-
-	const toggleDialog = props.toggleDialog;
-
-	const handleChange = name => event => {
-		setValues({ ...values, [name]: event.target.value });
+export class CreateBet extends Component {
+	state = {
+		title: '',
+		content: '',
 	};
 
-	return (
-		<React.Fragment>
-			<DialogTitle>Create Your Bet</DialogTitle>
-			<DialogContent>
-				<form classname="create-bet-form" autoComplete="on">
-					<TextField
-						id="filled-name"
-						label="Name"
-						className="bet-title"
-						value={values.name}
-						onChange={handleChange('name')}
-						margin="normal"
-						variant="filled"
-					/>
-				</form>
-			</DialogContent>
-			<DialogActions>
-				<Button onClick={toggleDialog} color="primary">
-					Save
-				</Button>
-				<Button onClick={toggleDialog} color="secondary">
-					Cancel
-				</Button>
-			</DialogActions>
-		</React.Fragment>
-	);
+	handleChange = e => {
+		this.setState({ [e.target.id]: e.target.value });
+		//! grabs the value from the id from the below JSX
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+		this.props.createProject(this.state);
+	};
+
+	render() {
+		const toggleDialog = this.props.toggleDialog;
+
+		return (
+			<React.Fragment>
+				<DialogTitle>Create Your Bet</DialogTitle>
+				<DialogContent>
+					<form classname="create-bet-form" onSubmit={this.handleSubmit}>
+						<TextField
+							id="filled-name"
+							label="Name"
+							className="bet-title"
+							onChange={this.handleChange}
+							margin="normal"
+							variant="filled"
+						/>
+					</form>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={toggleDialog} color="primary">
+						Save
+					</Button>
+					<Button onClick={toggleDialog} color="secondary">
+						Cancel
+					</Button>
+				</DialogActions>
+			</React.Fragment>
+		);
+	}
 }
+
+const mapDispatchToProps = dispatch => {
+	return {
+		createBet: bet => dispatch(createBet(bet)),
+	};
+};
+
+export default connect(
+	null,
+	mapDispatchToProps,
+)(CreateBet);
